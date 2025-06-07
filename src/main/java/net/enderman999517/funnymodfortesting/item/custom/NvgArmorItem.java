8f355059afe516eff1c352b692676fc6f485c7c6
Enderman999517 <jaspersismith@gmail.com>
2025-06-07 01:07:11 +0100

    lets go i didnt even need geckolib

--- src/main/java/net/enderman999517/funnymodfortesting/item/custom/NvgArmorItem.java
+++
@@ -1,4 +0,0 @@
-package net.enderman999517.funnymodfortesting.item.custom;
-
-public class NvgArmorItem implements {
-}
---
+++ src/main/java/net/enderman999517/funnymodfortesting/mixin/ItemRendererMixin.java
@@ -0,0 +1,25 @@
+package net.enderman999517.funnymodfortesting.mixin;
+
+import net.enderman999517.funnymodfortesting.FunnyModForTesting;
+import net.enderman999517.funnymodfortesting.item.ModItems;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(ItemRenderer.class)
+public abstract class ItemRendererMixin {
+    @ModifyVariable(method = "renderItem", at = @At(value = "HEAD"), argsOnly = true)
+    public BakedModel useNvgModel(BakedModel value, ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        if (stack.isOf(ModItems.NVG_GOGGLES) && renderMode != ModelTransformationMode.GUI) {
+            return ((ItemRendererAccessor) this).mccourse$getModels().getModelManager().getModel(new ModelIdentifier(FunnyModForTesting.MOD_ID, "nvg_3d", "inventory"));
+        }
+        return value;
+    }
+}
