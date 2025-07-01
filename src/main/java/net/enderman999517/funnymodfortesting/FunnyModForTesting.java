1157aaa60241139a8bf27f83f0c990b294376c1f
Enderman999517 <jaspersismith@gmail.com>
2025-07-01 22:15:10 +0100

    removed onInitialize comments

--- src/main/java/net/enderman999517/funnymodfortesting/FunnyModForTesting.java
+++ src/main/java/net/enderman999517/funnymodfortesting/FunnyModForTesting.java
@@ -25,9 +25,6 @@ public class FunnyModForTesting implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
 		ModBlocks.registerModBlocks();
 		ModItems.registerModItems();
